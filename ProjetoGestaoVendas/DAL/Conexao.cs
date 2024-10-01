f2b568afc3f255702116e1aa9063ab7d0a1a5136
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:24:56 -0300

    Create Conexao.cs

---
+++ ProjetoGestaoVendas/DAL/Conexao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProjetoGestaoVendas.DAL
+{
+    internal class Conexao
+    {
+        SqlConnection con = new SqlConnection("Data Source=WS-DANILO\\TEW_SQLEXPRESS;Initial Catalog=BDGESTAOVENDAS;Integrated Security=True;Encrypt=False");
+        public SqlConnection Conectar()
+        {
+            if(con.State == ConnectionState.Closed) { con.Open(); } 
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if(con.State == ConnectionState.Open) { con.Close(); }
+        }
+
+    }
+}
