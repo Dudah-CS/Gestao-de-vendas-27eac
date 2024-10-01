b4083fa5386276c74c233380d8c2c4d62e0027b9
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:54 -0300

    Create Cliente.cs

---
+++ ProjetoGestaoVendas/BLL/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class Cliente
+    {
+        int codcliente;
+        string nome;
+        string cpf;
+        string email;
+
+        public int Codcliente { get => codcliente; set => codcliente = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
