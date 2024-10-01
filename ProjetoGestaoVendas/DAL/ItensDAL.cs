1d2e1892a5420772f329b8daa21915a0b745287c
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:24:49 -0300

    Create ItensDAL.cs

---
+++ ProjetoGestaoVendas/DAL/ItensDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoGestaoVendas.DAL
+{
+    internal class ItensDAL
+    {
+        Conexao con = new Conexao();
+
+        public void Cadastrar(BLL.Itens itens)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"INSERT INTO Itens 
+            VALUES (@CodProduto, @CodVenda, @Quantidade,@Preco)";
+            cmd.Parameters.AddWithValue("@CodProduto", itens.Codproduto);
+            cmd.Parameters.AddWithValue("@CodVenda", itens.Codvenda);
+            cmd.Parameters.AddWithValue("@Quantidade", itens.Quantidade);
+            cmd.Parameters.AddWithValue("@Preco", itens.Preco);
+            cmd.Connection = con.Conectar();//abrir a conexão
+            cmd.ExecuteNonQuery();//executar o comando
+            con.Desconectar();
+        }
+    }
+}
