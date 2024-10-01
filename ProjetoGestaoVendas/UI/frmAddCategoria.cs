934bd8559dd27d81dc2ff4b94534ba2033b4bbc2
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:12:38 -0300

    Create frmAddCategoria.cs

---
+++ ProjetoGestaoVendas/UI/frmAddCategoria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoGestaoVendas.UI
+{
+    public partial class frmAddCategoria : Form
+    {
+        BLL.Categoria cat = new BLL.Categoria();
+        DAL.CategoriaDAL catDAL = new DAL.CategoriaDAL();
+
+        public int codcategoria { get; set; }
+
+        public frmAddCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            cat.Nomecategoria = txtNomeCategoria.Text;
+
+            if (codcategoria > 0)
+                catDAL.Atualizar(cat);
+            else
+                catDAL.Cadastrar(cat);
+
+            MessageBox.Show("Dados gravados com sucesso!");
+            Close();
+        }
+
+        private void frmAddCategoria_Load(object sender, EventArgs e)
+        {
+            if (codcategoria > 0) //editar
+            {
+                cat.Codcategoria = codcategoria;
+                cat = catDAL.Retornar(cat);
+                txtNomeCategoria.Text = cat.Nomecategoria;
+            }
+        }
+    }
+}
