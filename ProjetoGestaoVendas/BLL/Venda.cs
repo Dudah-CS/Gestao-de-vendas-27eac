0687740dc90e38d6c47011277fad9b709a37b39d
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:15 -0300

    Create Venda.cs

---
+++ ProjetoGestaoVendas/BLL/Venda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class Venda
+    {
+        int codvenda;
+        DateTime datavenda;
+        string formapagto;
+        int codcliente;
+        int codusuario;
+
+        public int Codvenda { get => codvenda; set => codvenda = value; }
+        public DateTime Datavenda { get => datavenda; set => datavenda = value; }
+        public string Formapagto { get => formapagto; set => formapagto = value; }
+        public int Codcliente { get => codcliente; set => codcliente = value; }
+        public int Codusuario { get => codusuario; set => codusuario = value; }
+    }
+}
