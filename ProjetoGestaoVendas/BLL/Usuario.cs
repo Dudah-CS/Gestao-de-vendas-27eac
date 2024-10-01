9c32c14fbed9557feaba4d5059dc936b33d2325f
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:19 -0300

    Create Usuario.cs

---
+++ ProjetoGestaoVendas/BLL/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class Usuario
+    {
+        int codusuario;
+        string nome;
+        string nomeusuario;
+        string senha;
+
+        public int Codusuario { get => codusuario; set => codusuario = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Nomeusuario { get => nomeusuario; set => nomeusuario = value; }
+        public string Senha { get => senha; set => senha = value; }
+    }
+}
