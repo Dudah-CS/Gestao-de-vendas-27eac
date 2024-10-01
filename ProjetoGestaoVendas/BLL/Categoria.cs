aa0c4c385440cab9edfb79b1a58cb655295c9780
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:58 -0300

    Create Categoria.cs

---
+++ ProjetoGestaoVendas/BLL/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class Categoria
+    {
+        int codcategoria;
+        string nomecategoria;
+
+        public int Codcategoria { get => codcategoria; set => codcategoria = value; }
+        public string Nomecategoria { get => nomecategoria; set => nomecategoria = value; }
+    }
+}
