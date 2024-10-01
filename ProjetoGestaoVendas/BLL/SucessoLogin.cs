955f3751cd174e61c8706301733a236618bb7df4
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:24 -0300

    Create SucessoLogin.cs

---
+++ ProjetoGestaoVendas/BLL/SucessoLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class SucessoLogin
+    {
+       
+       public bool Sucesso { get; set; }
+       public BLL.Usuario Usuario { get; set; }
+       public string Mensagem { get; set; }
+        
+
+    }
+}
