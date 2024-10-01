07d30b07e1ee66b25af78288268082c8de0621ff
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2024-10-01 15:25:49 -0300

    Create Itens.cs

---
+++ ProjetoGestaoVendas/BLL/Itens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGestaoVendas.BLL
+{
+    internal class Itens
+    {
+        int coditem;
+        int codproduto;
+        int codvenda;
+        int quantidade;
+        double preco;
+
+        public int Coditem { get => coditem; set => coditem = value; }
+        public int Codproduto { get => codproduto; set => codproduto = value; }
+        public int Codvenda { get => codvenda; set => codvenda = value; }
+        public int Quantidade { get => quantidade; set => quantidade = value; }
+        public double Preco { get => preco; set => preco = value; }
+    }
+}
